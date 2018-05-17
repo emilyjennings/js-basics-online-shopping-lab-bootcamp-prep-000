400de6612af18e6cd93eab72e6e66d4a5deef69f
Emily Jennings <emily.jennings@flatironschool.com>
2018-05-17 15:26:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,6 +23,15 @@ function addToCart(item) {
 }
 */
 
+
+function addToCart (item) {
+  var price = Math.floor(100*(Math.random()));
+  var itemObj = {[item]: price};
+  cart.push(itemObj);
+  console.log(`${item} has been added to your cart.`);
+  return cart;
+}
+
 function viewCart() {
   // write your code here
 }
